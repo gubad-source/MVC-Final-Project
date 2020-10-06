0daa433beebf92ed1773ec0e764fa5d8a6ee948b
gubad-source <qubadmustafayev@gmail.com>
2020-10-06 21:00:30 +0400

    Project have been added

---
+++ MvcFinalProject/MvcFinalProject/Models/HomePortfolioItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcFinalProject.Models
+{
+    public class HomePortfolioItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Heading { get; set; }
+        public string Photo { get; set; }
+    }
+}
---
+++ MvcFinalProject/MvcFinalProject/Models/HomeTeamItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcFinalProject.Models
+{
+    public class HomeTeamItem
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public string Content { get; set; }
+        public string Photo { get; set; }
+        public string Facebook { get; set; }
+        public string Linkedin { get; set; }
+
+        public string Dribble { get; set; }
+    }
+}
